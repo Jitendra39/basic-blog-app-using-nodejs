cf962c29f91f19067ae5bb8e3b74a92a721ab04c
Jitendra39 <159651421+Jitendra39@users.noreply.github.com>
2024-07-11 21:52:11 +0530

    Update index.js

--- index.js
+++ index.js
@@ -2,47 +2,50 @@ const express = require('express');
 const app = express();
 require('dotenv').config();
 const path = require('path');
-const mongoose = require("mongoose");
+const mongoose = require('mongoose');
 const userRoute = require('./routes/user');
-const blogRoute = require("./routes/blog.js");
+const blogRoute = require('./routes/blog.js');
 const cookieParser = require('cookie-parser');
 const Blog = require('./models/blog.js');
 const { checkForAuthenticationCookie } = require('./middleware/authentication');
 
 const port = process.env.PORT || 8000;
 
-//$env:MONGO_URL = "mongodb://localhost:27017/boggers"
-
-//'mongodb://localhost:27017/boggers'
-mongoose.connect(process.env.MONGO_URL).then(() =>{
-    useNewUrlParser: true,
+// Connect to MongoDB
+mongoose.connect(process.env.MONGO_URL, {
+  useNewUrlParser: true,
   useUnifiedTopology: true,
   connectTimeoutMS: 30000,
   socketTimeoutMS: 45000 
-  console.log("mongodb is connected")
-})
+}).then(() => {
+  console.log("MongoDB is connected");
+}).catch(err => {
+  console.error("Error connecting to MongoDB:", err);
+});
 
 app.set('view engine', 'ejs');
 app.set("views", path.resolve("./views"));
 
-app.use(express.urlencoded({ extended: false}))
+app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(checkForAuthenticationCookie("token"))
-app.use(express.static(path.resolve('./public')))
+app.use(checkForAuthenticationCookie("token"));
+app.use(express.static(path.resolve('./public')));
 
-app.use('/user', userRoute)
-app.use('/blog', blogRoute)
+app.use('/user', userRoute);
+app.use('/blog', blogRoute);
 
-app.get('/', async(req,res) =>{
-
-  const allBlogs = await Blog.find({})
-
-  res.render("home",{
-    user: req.user,
-    blogs: allBlogs
-  });
-})
-
+app.get('/', async (req, res) => {
+  try {
+    const allBlogs = await Blog.find({});
+    res.render("home", {
+      user: req.user,
+      blogs: allBlogs
+    });
+  } catch (err) {
+    console.error("Error fetching blogs:", err);
+    res.status(500).send("Internal Server Error");
+  }
+});
 
 app.listen(port, () => {
   console.log(`Server is running on port ${port}`);
